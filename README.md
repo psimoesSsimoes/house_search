b5bdacabc33932e73f0a6b39db113d0f8c27bc6e
pedro <p.simoes@campus.fct.unl.pt>
2017-01-26 13:41:56 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,28 @@
+#     -  House Search - 
+#### your own real-estate guy.inside.your.computer!
+---
+
+### Description
+
+provided an link with from olx or sapo.casas, for a spefic location (let's say for example vendas novas in Evora: https://www.olx.pt/imoveis/vendasnovas-evora/?search[description]=1&search[order]=filter_float_price%)
+it emails you with an suggestion of houses for you to check.
+
+put it under a cron job, and you'll control
+
+![alt text](https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQPI2ftgKS4tk4J2PMEyg7HBrdF7UBuG4cWPe8GV5SduwI0ImJl "Logo Title Text 1")
+
+
+
+
+
+## Build on top of
+[Python](https://www.python.org/)
+[SendMail](http://www.sendmail.com/sm/open_source/)
+
+## set up and run
+1. Open File
+2. change link on getHouses
+3. change you variable to your email
+4. python mail.py
+
+#### Any questions/suggestions email me: @simoes
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from lxml import html
 import requests
+import re
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from django.utils.encoding import smart_str, smart_unicode
 from subprocess import Popen, PIPE
 
 
 
+def convert(s):
+    try:
+        return s.group(0).encode('latin1').decode('utf8')
+    except:
+        return s.group(0)
+
+
 def getHouses(link,website):
     page = requests.get(link)
-    tree = html.fromstring(page.content)
+    tree = html.fromstring(page.text)
     finalList=[]
 
     if 'olx' in website:
@@ -16,7 +26,8 @@ def getHouses(link,website):
         area = tree.xpath('/html/body/div[1]/div[2]/section/div[3]/div/div[1]/table/tbody/tr/td/table/tbody/tr[2]/td[1]/div/p[2]/text()')
         href = tree.xpath('/html/body/div[1]/div[2]/section/div[3]/div/div[1]/table/tbody/tr/td/table/tbody/tr[1]/td[2]/div/h3/a/@href')
         for i in range(len(prices)):
-            finalList.append((names[i],prices[i],area[i].strip(),'https://www.olx.pt'+href[i]))
+            finalList.append([names[i],prices[i],area[i].strip(),href[i]])
+	
         return finalList
 
     elif 'sapo' in website:
@@ -27,7 +38,7 @@ def getHouses(link,website):
         href.pop(0)
         href.pop(0)
         for i in range(len(names)):
-            finalList.append((names[i],description[i].strip(),price[i],'http://casa.sapo.pt'+href[i]))
+            finalList.append([names[i],description[i].strip(),price[i],href[i]])
         return finalList
 
     else:
@@ -38,10 +49,44 @@ finalmsg += getHouses('https://www.olx.pt/imoveis/casas-moradias-para-arrendar-v
 finalmsg += getHouses('https://www.olx.pt/imoveis/quartos-para-aluguer/caparica/?search%5Bdescription%5D=1','olx')
 finalmsg += getHouses('http://casa.sapo.pt/Alugar/Apartamentos/T0-ate-T2/Almada/Costa-da-Caparica/?sa=15','sapo')
 
+finalstr=''
+for i in finalmsg:
+	try:
+		print (re.sub(r'[\x80-\xFF]+', convert, i[0]))
+		finalstr+='<h1>'+re.sub(r'[\x80-\xFF]+', convert,i[0])+'</h1>'
+		finalstr+='<h2>'+re.sub(r'[\x80-\xFF]+', convert,i[1])+'</h2>'
+		for j in range(2,4):
+			print (re.sub(r'[\x80-\xFF]+', convert,i[j])) 
+	        	finalstr+="<p>"+re.sub(r'[\x80-\xFF]+', convert,i[j])+"</p>"
+	except UnicodeEncodeError:
+		print ("did exceptionnnnnnnnnnnnnnn")
+		finalstr+='<h1>Casa com encoding Malefico</h1>'
+                for j in range(1,4):
+                        print (re.sub(r'[\x80-\xFF]+', convert,i[j]))
+                        finalstr+="<p>"+re.sub(r'[\x80-\xFF]+', convert,i[j])+"</p>"
 
-msg = MIMEText(finalmsg)
-msg["From"] = "my.real.state.agent@fitman.com"
-msg["To"] = "p.simoes@campus.fct.unl.pt"
-msg["Subject"] = "Houses list for today."
+
+finalstr = finalstr.encode('utf8')  
+me = "my.real.state.agent@myownserver.com"
+you = "p.simoes@campus.fct.unl.pt"
+
+# Create message container - the correct MIME type is multipart/alternative.
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Today's House List"
+msg['From'] = me
+msg['To'] = you
+
+html = """\
+<html>
+  <head></head>
+  <body>
+        """+finalstr+""" </body>
+</html>
+"""
+
+part2 = MIMEText(html, 'html')
+msg.attach(part2)
+
 p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
 p.communicate(msg.as_string())
+
